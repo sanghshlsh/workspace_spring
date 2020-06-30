e0cc322599c1c563062aa47ecd0a52b73ad1cdc1
sanghshlsh <sanghshlsh@gmail.com>
2020-06-30 15:32:06 +0900

    06-30

--- World/src/main/java/com/naver/controller/MemberController.java
+++ World/src/main/java/com/naver/controller/MemberController.java
@@ -3,13 +3,19 @@ package com.naver.controller;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 
 import kr.co.service.MemberService;
 
 @Controller
-@RequestMapping("member")
+@RequestMapping("member")//이를 추가함으로서 insertjsp접근방법은 member/insert가 되었다
 public class MemberController {
 	
 	@Autowired//service인터페이스를 구현한 클래스에 @service어노테이션이 없다면 이곳에 inject/autowired어노테이션이 memberservice객체를 가져오지 못한다.
 	private MemberService memberService;
+	
+	@RequestMapping(value = "/insert", method = RequestMethod.GET)
+	public String insert() {
+		return "member/insert";
+	}
 }
