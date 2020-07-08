1655e114c79d69b76650bde4388572054fd0c9b4
sanghshlsh <sanghshlsh@gmail.com>
2020-07-08 14:32:13 +0900

    07-08

--- World8/src/main/java/kr/co/ca/UploadController.java
+++ World8/src/main/java/kr/co/ca/UploadController.java
@@ -2,6 +2,9 @@ package kr.co.ca;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.multipart.MultipartFile;
+import org.springframework.web.multipart.MultipartRequest;
 
 @Controller
 public class UploadController {
@@ -9,5 +12,14 @@ public class UploadController {
 	@RequestMapping(value = "/uploadform")
 	public void uploadform() {
 		
+	}
+	
+	@RequestMapping(value = "/uploadform", method = RequestMethod.POST)
+	public void uploadform(MultipartRequest request) {
+		MultipartFile file = request.getFile("file");
+		
+		System.out.println(file.getOriginalFilename());
+		System.out.println(file.getSize());
+		System.out.println(file.getContentType());
 	}
 }
