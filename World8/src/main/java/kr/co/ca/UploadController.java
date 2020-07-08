e57d12191f52b283b4134a5e9d4ad956ff5569db
sanghshlsh <sanghshlsh@gmail.com>
2020-07-08 14:46:21 +0900

    07-08

--- World8/src/main/java/kr/co/ca/UploadController.java
+++ World8/src/main/java/kr/co/ca/UploadController.java
@@ -1,6 +1,10 @@
 package kr.co.ca;
 
+import java.io.File;
+import java.io.IOException;
+
 import org.springframework.stereotype.Controller;
+import org.springframework.util.FileCopyUtils;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.multipart.MultipartFile;
@@ -15,11 +19,14 @@ public class UploadController {
 	}
 	
 	@RequestMapping(value = "/uploadform", method = RequestMethod.POST)
-	public void uploadform(MultipartRequest request) {
+	public void uploadform(MultipartRequest request) throws Exception {
 		MultipartFile file = request.getFile("file");
 		
-		System.out.println(file.getOriginalFilename());
-		System.out.println(file.getSize());
-		System.out.println(file.getContentType());
+		System.out.println("orgName : "+file.getOriginalFilename());
+		System.out.println("size : "+file.getSize());
+		System.out.println("contentType : "+file.getContentType());
+			
+		File target = new File("C:"+File.separator+"upload", file.getOriginalFilename() );
+		FileCopyUtils.copy(file.getBytes(), target);	//	파일 업로드 코드	//file에는 문자열을 직접 넣어도 된다.
 	}
 }
